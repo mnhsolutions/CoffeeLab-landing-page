cda87f8e02c8b6cdac338a90898388d2375781dc
coronelhernan <hcoronel1996@gmail.com>
2026-02-02 22:02:15 -0300

    feat: configuration initial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-indigo-500">
+        Inicio de proyecto
+      </h1>
+    </div>
+  )
+}
